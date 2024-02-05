cb371018df8e6bb342493d602e5402aa5f1b1611
duzhg56980 <guokey1996@gmail.com>
2024-02-05 15:40:26 +0800

    init commit

---
+++ 473271-0.9.30/FunctionUse1396.java
@@ -0,0 +1,6 @@
+import ch.qos.logback.classic.util.StatusListenerConfigHelper;
+public class FunctionUse1396 {
+public void funcUse() {
+StatusListenerConfigHelper statuslistenerconfighelper = new StatusListenerConfigHelper();
+}
+}
